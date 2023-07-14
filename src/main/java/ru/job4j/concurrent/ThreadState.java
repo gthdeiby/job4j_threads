41aabc4e24b44071037d5a50fb7fe52bb5f80ab5
dim0nster <32572277+dim0nster@users.noreply.github.com>
2023-07-14 14:43:57 +0300

    3. Состояние нити. [#229175 #429941]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -12,8 +12,8 @@ public class ThreadState {
         System.out.println("Second - " + second.getState());
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED ||
-                second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
             System.out.println("First - " + first.getState());
             System.out.println("Second - " + second.getState());
         }

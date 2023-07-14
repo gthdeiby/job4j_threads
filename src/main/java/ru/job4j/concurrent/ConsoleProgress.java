e865956659e9b71ec2af4f8877054d379b3c4c45
dim0nster <32572277+dim0nster@users.noreply.github.com>
2023-07-15 01:53:19 +0300

    6. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -13,7 +13,7 @@ public class ConsoleProgress implements Runnable {
                 index = index == process.length ? 0 : index;
             }
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 

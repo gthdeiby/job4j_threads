ec199289302b1f53af758825af8c4c88c024faee
dim0nster <32572277+dim0nster@users.noreply.github.com>
2023-07-28 22:34:56 +0300

    8. Скачивание файла с ограничением. [#144271 #432594]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,6 +1,5 @@
 package ru.job4j.thread;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -8,8 +7,10 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 public class Wget implements Runnable {
+    private static final int TIME = 1000;
     private final String url;
     private final int speed;
+    private static String fileName;
 
     public Wget(String url, int speed) {
         this.url = url;
@@ -17,7 +18,7 @@ public class Wget implements Runnable {
     }
 
     public static void validateArgs(String[] args) throws MalformedURLException, URISyntaxException {
-        if (args.length != 2) {
+        if (args.length != 3) {
             throw new IllegalArgumentException("Argument pattern violation");
         }
 
@@ -38,19 +39,25 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-        var file = new File("tmp.xml");
         try (var in = new URL(url).openStream();
-            var out = new FileOutputStream(file)) {
+            var out = new FileOutputStream(fileName)) {
             var dataBuffer = new byte[1024];
             int bytesRead;
+            int bytesCount = 0;
             var startAt = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
-                var downloadAt = System.currentTimeMillis();
+                bytesCount += bytesRead;
                 out.write(dataBuffer, 0, bytesRead);
-                if (downloadAt - startAt < speed) {
-                    Thread.sleep(speed - downloadAt + startAt);
+                if (bytesCount > speed) {
+                    var speedTime = System.currentTimeMillis() - startAt;
+                    if (speedTime < TIME) {
+                        Thread.sleep(TIME - speedTime);
+                    }
+                    bytesCount = 0;
+                    startAt = System.currentTimeMillis();
                 }
-                startAt = System.currentTimeMillis();
+
+
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -61,6 +68,7 @@ public class Wget implements Runnable {
         validateArgs(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
+        fileName = args[2];
         Thread wget = new Thread(new Wget(url, speed));
         wget.start();
         wget.join();

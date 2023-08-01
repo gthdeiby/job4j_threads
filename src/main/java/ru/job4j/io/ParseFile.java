1fc32483d667ce75b1fe71e944df87cfb4c942db
dim0nster <32572277+dim0nster@users.noreply.github.com>
2023-08-01 16:51:52 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102 #433740] fix

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -11,17 +11,21 @@ public final class ParseFile {
         this.file = file;
     }
 
-    public synchronized String getContent(Predicate<Integer> filter) throws IOException {
+    private synchronized String getContent(Predicate<Integer> filter) throws IOException {
         StringBuilder output = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             int data;
-            while ((data = reader.read()) > 0) {
+            while ((data = reader.read()) != -1) {
                 if (filter.test(data)) {
                     output.append((char) data);
                 }
             }
         }
         return output.toString();
+    }
+
+    public synchronized String getContentWithUnicode(Predicate<Integer> filter) throws IOException {
+        return getContent(ch -> true);
     }
 
     public synchronized String getContentWithoutUnicode(Predicate<Integer> filter) throws IOException {
